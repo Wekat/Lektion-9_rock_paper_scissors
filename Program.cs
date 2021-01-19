3411a8d230877803d949471f50206fb7299f05fe
Wekat <60980395+Wekat@users.noreply.github.com>
2021-01-19 11:31:13 +0100

    Update Program.cs
    switch case for choosing hand

--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace Lektion_9_rock_paper_scissors
             Console.WriteLine("Rules: You choose between Rock, Paper or Scissors.");
             Console.WriteLine("The computer will randomly do the same.\n");
             Console.WriteLine("How you win: Rock wins Scissors, Paper wins Rock and Scissors wins Paper.");
+
+            bool continuePlay = true;
+
+            do
+            {
+                Choose:
+                Console.WriteLine("\nChoose your hand!");
+                Console.WriteLine("Rock [1]\nPaper [2]\nScissors [3]");
+                int chosenNum = Convert.ToInt32(Console.ReadLine());
+
+                string chosenHand;
+
+                switch (chosenNum)
+                {
+                    case 1:
+                    chosenHand = "Rock";
+                    break;
+                    case 2:
+                    chosenHand = "Paper";
+                    break;
+                    case 3:
+                    chosenHand = "Scissors";
+                    break;
+                    default:
+                    Console.WriteLine("That was an invalid entry. Please try again.\n");
+                    goto Choose;
+                }
+                Console.WriteLine("\nYour chosen hand is: " + chosenHand);
+            } while (continuePlay == true);
         }
     }
 }
