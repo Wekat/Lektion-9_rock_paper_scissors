1a40b115e63ab0a5008b82c2f5bd3c8346ede080
Wekat <60980395+Wekat@users.noreply.github.com>
2021-01-19 11:16:29 +0100

    Added rules

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lektion_9_rock_paper_scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the wonderful game of Rock/Paper/Scissors!\n");
+            Console.WriteLine("Rules: You choose between Rock, Paper or Scissors.");
+            Console.WriteLine("The computer will randomly do the same.\n");
+            Console.WriteLine("How you win: Rock wins Scissors, Paper wins Rock and Scissors wins Paper.");
+        }
+    }
+}
